2f85aecaa9305b28b1ff3d6662a552bda0966efe
Bartlomiej Oscislowski <bartlomiej.oscislowski@gmail.com>
2020-04-10 00:04:27 +0200

    Added dal, bll, common library projects Directory schema

---
+++ MyITShop.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyITShop.Common
+{
+    public class Class1
+    {
+    }
+}
